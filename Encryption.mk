6467bbdf9b3e62faa3d668f267242a4cffc1d894
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-06 18:42:03 -0500

    Define Encryption Targets

---
+++ Encryption.mk
@@ -0,0 +1,22 @@
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+
+# Recovery modules
+TARGET_RECOVERY_DEVICE_MODULES += \   
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+#check your keymaster before use it android 12 use 4.1 android 11 use 4 keymaster service is needed in both of them
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_USE_FSCRYPT_POLICY := 2
+
+
