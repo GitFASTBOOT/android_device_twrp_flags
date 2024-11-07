ff37e4925a707a40779868061d54e87d81e825d6
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-06 19:21:19 -0500

    manage Targets

--- Recovery.mk
+++ Recovery.mk
@@ -12,6 +12,14 @@ PLATFORM_VERSION := 16.1.0
 
 TARGET_USES_MKE2FS := true
 
+# Verified Boot (this basic avb flags so you might need to add more)
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
 TW_THEME := portrait_hdpi   #use this if you use phone not tablet
 TW_EXTRA_LANGUAGES := false
 TW_SCREEN_BLANK_ON_BOOT := true
@@ -38,3 +46,26 @@ TWRP_INCLUDE_LOGCAT := true
 
 # Storage
 RECOVERY_SDCARD_ON_DATA := true
+
+#you need to define this 
+# Architecture
+#TARGET_ARCH := arm64
+#TARGET_ARCH_VARIANT := armv8-a
+#TARGET_CPU_ABI := arm64-v8a
+#TARGET_CPU_ABI2 := 
+#TARGET_CPU_VARIANT := generic
+#TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+#TARGET_2ND_ARCH := arm
+#TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+#TARGET_2ND_CPU_ABI := armeabi-v7a
+#TARGET_2ND_CPU_ABI2 := armeabi
+#TARGET_2ND_CPU_VARIANT := generic
+#TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Platform
+#TARGET_BOARD_PLATFORM := mt6768 #see your board codename
+
+
+
+
